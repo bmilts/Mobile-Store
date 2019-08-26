e7da4eb1f7d38edd376a574b543bcca65c95dfcf
Brendan Milton <mirrorimage101@hotmail.com>
2019-08-26 15:39:27 +0100

    Added admin code to pick image, convert it to data and upload the image via url to firestore

--- EcommerceAdmin/Controller/AddEditCategoryVC.swift
+++ EcommerceAdmin/Controller/AddEditCategoryVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseStorage
+import FirebaseFirestore
 
 class AddEditCategoryVC: UIViewController {
     @IBOutlet weak var nameText: UITextField!
@@ -29,6 +31,81 @@ class AddEditCategoryVC: UIViewController {
     }
     
     @IBAction func addCategoryClicked(_ sender: Any) {
+        activityIndicator.startAnimating()
+        uploadImageThenDocument()
+    }
+    
+    func uploadImageThenDocument() {
+         
+        // Check fields are not null
+        guard let image = categoryImage.image ,
+            let categoryName = nameText.text , categoryName.isNotEmpty else {
+                simpleAlert(title: "Error", message: "Please add category image and name")
+                return
+        }
+        
+        // Step 1: Turn the image into data and compress image
+        guard let imageData = image.jpegData(compressionQuality: 0.2) else { return }
+        
+        // Step 2: Create a storage image reference -> A location in Firestorage for to store
+        let imageRef = Storage.storage().reference().child("/categoryImages/\(categoryName).jpg")
+        
+        // Step 3: Set the meta data to assist classification of files
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpg"
+        
+        // Step 4: Upload data
+        imageRef.putData(imageData, metadata: metaData) { (metaData, error) in
+            
+            if let error = error {
+                self.handleError(error: error, msg: "Unable to upload image.")
+                return
+            }
+            
+            // Step 5: Once the image is uploaded, retrieve the download URL
+            imageRef.downloadURL { (url, error) in
+                
+                if let error = error {
+                    self.handleError(error: error, msg: "Unable to retrieve image url.")
+                    return
+                }
+                
+                guard let url = url else { return }
+                
+                // Step 6: Upload new category document to the Firestore categories collection
+                self.uploadDocument(url: url.absoluteString)
+            }
+
+        }
+    
+    }
+    
+    func uploadDocument(url: String) {
+        var docRef: DocumentReference!
+        var category = Category.init(name: nameText.text!, id: "", imgUrl: url, timeStamp: Timestamp())
+        
+        docRef = Firestore.firestore().collection("categories").document()
+        category.id = docRef.documentID
+        
+        let data = Category.modelToData(category: category)
+        
+         // Editing or adding brand new category
+        docRef.setData(data, merge: true) { (error) in
+            if let error = error {
+                self.handleError(error: error, msg: "Unable to upload image")
+                return
+            }
+            
+            // Back to categories
+            self.navigationController?.popViewController(animated: true)
+
+        }
+    }
+    
+    func handleError(error: Error, msg: String) {
+        debugPrint(error.localizedDescription)
+        self.simpleAlert(title: "Error", message: msg)
+        self.activityIndicator.stopAnimating()
     }
 }
 
--- Shared/Model/Category.swift
+++ Shared/Model/Category.swift
@@ -16,11 +16,40 @@ struct Category {
     var isActive: Bool = true
     var timeStamp: Timestamp
     
+    // Default initializer
+    init(
+        name: String,
+        id: String,
+        imgUrl: String,
+        isActive: Bool = true,
+        timeStamp: Timestamp) {
+        
+        self.name = name
+        self.id = id
+        self.imgUrl = imgUrl
+        self.isActive = isActive
+        self.timeStamp = timeStamp
+    }
+    
+    // Data parsing initializer
     init(data: [String : Any]) {
         self.name = data["name"] as? String ?? ""
         self.id = data["id"] as? String ?? ""
         self.imgUrl = data["imgUrl"] as? String ?? ""
         self.isActive = data["isActive"] as? Bool ?? true
         self.timeStamp = data["timeStamp"] as? Timestamp ?? Timestamp()
+    }
+    
+    // Convert JSON data model to key value pair for firestore
+    static func modelToData(category: Category) -> [String: Any] {
+        let data : [String: Any] = [
+            "name": category.name,
+            "id": category.id,
+            "imgUrl": category.imgUrl,
+            "isActive": category.isActive,
+            "timeStamp": category.timeStamp
+        ]
+        
+        return data
     }
 }
